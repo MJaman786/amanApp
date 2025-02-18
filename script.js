ecd91a6fe3f38beac7aab899896f840a00ff9fab
MJaman786 <112769441+MJaman786@users.noreply.github.com>
2025-02-18 16:35:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let taskList = [];
+
+function addTasks(){
+    let textBox = document.querySelector('#textBox');
+    let dateBox = document.querySelector('#dateBox');
+
+    let text = textBox.value;
+    let date = dateBox.value;
+
+    if (text && date) { // Ensure the input fields are not empty
+        taskList.push({ text, date });
+
+        textBox.value = '';
+        dateBox.value = '';
+
+        savetoLocalstorage();
+        display();
+    } else {
+        alert("Please fill in both task and date!");
+    }
+}
+
+function display(){
+    let taskListContainer = document.querySelector('#taskList');
+    taskListContainer.innerHTML = '';
+
+    for(let i=0; i<taskList.length; i++){
+        let newTask = document.createElement('div');
+        newTask.className = 'newTaskCtn';
+        newTask.innerHTML = `
+            <span>
+                <span class="taskName">${taskList[i].text}</span>
+                <span class="taskDate">${taskList[i].date}</span>
+                <button class="clsBtn" onclick="removeTask(${i});">Clear</button>
+            </span>
+        `;
+        taskListContainer.appendChild(newTask);
+    }
+}
+
+function removeTask(index){
+    taskList.splice(index,1);
+    savetoLocalstorage();
+    display();
+}
+
+function savetoLocalstorage(){
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+}
+
+function loadfromLocalstorage(){
+    taskList = JSON.parse(localStorage.getItem('taskList'));
+    display();
+}
+
+window.onload = loadfromLocalstorage;
